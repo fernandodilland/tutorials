fb81a12317cf930e9b7a5742bbb01078214e0dbc
Fernando Mireles <fernandodilland@gmail.com>
2021-06-22 01:05:09 -0500

    Add files via upload

---
+++ Tutorial.py
@@ -0,0 +1,9 @@
+def funcionDato():
+    print("--- Se ingresó a la función ---")
+    edad = input("Ingrese su edad: ")
+    print("Su edad es:", edad)
+    print("--- Se salió de la función ---")
+
+print("Bienvenido(a) al programa")
+print("--- Se manda a llamar la función ---")
+funcionDato()
